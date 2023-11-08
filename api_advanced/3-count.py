57aa0af692eea22f5cd702acbf27b7e879e2af28
m-mwangi <127857750+m-mwangi@users.noreply.github.com>
2023-11-08 12:30:19 +0200

    Update 3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and counts occurrences of given keywords in hot articles' titles
+Queries the Reddit API recursively and prints the count of given keywords
 """
 import requests
 
@@ -9,34 +9,37 @@ def count_words(subreddit, word_list, after=None, counts=None):
         counts = {}
         for word in word_list:
             counts[word.lower()] = 0
-
+    
     headers = {"User-Agent": "Mozilla/5.0"}
     params = {"after": after, "limit": 100}
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    
     if response.status_code != 200:
         return
-
+    
     data = response.json().get("data", {}).get("children", [])
-
+    
     if not data:
         sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
         for word, count in sorted_counts:
             if count > 0:
                 print(f"{word}: {count}")
         return
-
+    
     for post in data:
-        title = post.get("data", {}).get("title", "").lower().split()
+        title = post.get("data", {}).get("title", "").lower()
         for word in word_list:
-            counts[word.lower()] += title.count(word.lower())
+            word_lower = word.lower()
+            counts[word_lower] += title.count(word_lower)
 
     after = data[-1].get("data", {}).get("name")
     count_words(subreddit, word_list, after, counts)
 
 if __name__ == "__main__":
     import sys
+    
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
         print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
