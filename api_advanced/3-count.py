f182ad4c8ed16bd80e3f843a2298921ff65476c8
m-mwangi <127857750+m-mwangi@users.noreply.github.com>
2023-11-08 12:28:25 +0200

    Update 3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,44 +1,46 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of subscribers
-for a given subreddit
+Queries the Reddit API and counts occurrences of given keywords in hot articles' titles
 """
 import requests
 
 def count_words(subreddit, word_list, after=None, counts=None):
     if counts is None:
-        counts = {word.lower().strip(): 0 for word in word_list}
-    
+        counts = {}
+        for word in word_list:
+            counts[word.lower()] = 0
+
     headers = {"User-Agent": "Mozilla/5.0"}
     params = {"after": after, "limit": 100}
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
     if response.status_code != 200:
-        return None
-    
+        return
+
     data = response.json().get("data", {}).get("children", [])
-    
+
     if not data:
-        return counts
-    
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            if count > 0:
+                print(f"{word}: {count}")
+        return
+
     for post in data:
-        title = post.get("data", {}).get("title", "").lower()
+        title = post.get("data", {}).get("title", "").lower().split()
         for word in word_list:
-            word_lower = word.lower().strip()
-            counts[word_lower] += title.count(word_lower)
+            counts[word.lower()] += title.count(word.lower())
 
     after = data[-1].get("data", {}).get("name")
-    return count_words(subreddit, word_list, after, counts)
+    count_words(subreddit, word_list, after, counts)
 
 if __name__ == "__main__":
-    subreddit = input("Enter subreddit: ").strip()
-    words = input("Enter words (comma-separated): ").split(',')
-    word_counts = count_words(subreddit, words)
-    
-    if word_counts is None:
-        print(f"Invalid subreddit: {subreddit}")
+    import sys
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
     else:
-        for word, count in word_counts.items():
-            print(f"{word.strip()}: {count}")
+        subreddit = sys.argv[1]
+        words = sys.argv[2:]
+        count_words(subreddit, words)
