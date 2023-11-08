e171c7b5923f5b68e3e88944492e9e7346019179
m-mwangi <127857750+m-mwangi@users.noreply.github.com>
2023-11-08 13:01:01 +0200

    Update 3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -9,34 +9,38 @@ def count_words(subreddit, word_list, after=None, word_count=None):
     if word_count is None:
         word_count = {}
 
-    if after is None:
-        url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    else:
-        url = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(subreddit, after)
+    try:
+        if after is None:
+            url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+        else:
+            url = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(subreddit, after)
 
-    headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+        headers = {'User-agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
-        return
+        response.raise_for_status()  # Raise HTTPError for bad requests
 
-    data = response.json()
-    posts = data['data']['children']
+        data = response.json()
+        posts = data['data']['children']
 
-    for post in posts:
-        title = post['data']['title'].lower().split()
-        for word in word_list:
-            word_count[word.lower()] = word_count.get(word.lower(), 0) + title.count(word.lower())
+        for post in posts:
+            title = post['data']['title'].lower().split()
+            for word in word_list:
+                word_count[word.lower()] = word_count.get(word.lower(), 0) + title.count(word.lower())
 
-    after = data['data']['after']
+        after = data['data']['after']
 
-    if after is not None:
-        count_words(subreddit, word_list, after, word_count)
-    else:
-        sorted_word_count = sorted(word_count.items(), key=lambda x: (-x[1], x[0].lower()))
-        for word, count in sorted_word_count:
-            print('{}: {}'.format(word.lower(), count))
+        if after is not None:
+            count_words(subreddit, word_list, after, word_count)
+        else:
+            sorted_word_count = sorted(word_count.items(), key=lambda x: (-x[1], x[0].lower()))
+            for word, count in sorted_word_count:
+                print('{}: {}'.format(word.lower(), count))
 
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+    except KeyError as e:
+        print(f"Unexpected response format: {e}")
 
 if __name__ == '__main__':
     count_words('programming', ['react', 'python', 'java', 'javascript', 'scala', 'no_results_for_this_one'])
